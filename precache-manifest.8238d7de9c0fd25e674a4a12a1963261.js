178edbe6ef832ab0f0ca15454bbda84cfc616a61
ariel_fenster <arielfenster1610@gmail.com>
2020-08-13 17:48:03 +0300

    Updates

---
+++ precache-manifest.8238d7de9c0fd25e674a4a12a1963261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9488743bb1f99398c3796146e65bdc",
+    "url": "/whatdoesitallmean/index.html"
+  },
+  {
+    "revision": "757ca1138e484d875dd5",
+    "url": "/whatdoesitallmean/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d72109dae5767e47ce82",
+    "url": "/whatdoesitallmean/static/js/2.0a3a957e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whatdoesitallmean/static/js/2.0a3a957e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757ca1138e484d875dd5",
+    "url": "/whatdoesitallmean/static/js/main.5142a2ba.chunk.js"
+  },
+  {
+    "revision": "7bc3688e917e5117de83",
+    "url": "/whatdoesitallmean/static/js/runtime-main.1b8a4260.js"
+  }
+]);
